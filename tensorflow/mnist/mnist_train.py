5442aa3f08289a8359667d06202c849052a9b4ce
msyksphinz <msyksphinz.dev@gmail.com>
2015-12-06 12:10:15 +0000

    Func: temporary implement mnist

--- tensorflow/mnist/mnist_train.py
+++ tensorflow/mnist/mnist_train.py
@@ -10,30 +10,15 @@ init = tf.initialize_all_variables()
 sess = tf.Session()
 sess.run(init)
 
-batch_xs, batch_ys = mnist.train.next_batch(1)
-for y in range(28):
-      for x in range(28):
-            if (batch_xs[0][y*28+x] != 0):
-                  print "1",
-            else:
-                  print "0",
-      print ""
-      
 
-for idx in range(10):      
+
+img_gen_run = sess.run(img_gen)
+for idx in range(10):
       for y in range(IMG_WIDTH):
             for x in range(IMG_WIDTH):
-                  print sess.run(img_gen)[idx][IMG_WIDTH*y+x],
+                  print img_gen_run[idx][IMG_WIDTH*y+x],
             print ""
-      
 
-
-                      
-
-      
 for i in range(1000):
       batch_xs, batch_ys = mnist.train.next_batch(1)
-      
-
-#      sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
-      
+#     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
