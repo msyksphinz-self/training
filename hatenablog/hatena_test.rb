66e6605dc3b01db9dc5858b9019587ec686c595b
Masayuki Kimura <masayuki.kimura.1986@gmail.com>
2015-12-19 14:05:24 +0000

    Func: add hatena_test.rb

---
+++ hatenablog/hatena_test.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+# -*- encoding: utf-8 -*-
+
+require 'atomutil'
+
+POST_URI = "https://blog.hatena.ne.jp/msyksphinz/msyksphinz.hatenablog.com/atom/entry"
+
+auth = Atompub::Auth::Wsse.new(
+  username: 'msyksphinz',
+  password: 'l5l3j1mx7h'
+)
+client = Atompub::Client.new(auth: auth)
+
+feed = client.get_feed(POST_URI);
+
+out_fp = File.open("output.txt", "w")
+
+while feed != nil do
+  entry = feed.entries
+  entry.each{|content|
+    out_fp.printf("%s\n", content.title)
+    # out_fp.printf("%s\n", content.content)
+  }
+
+  if feed.next_link == nil then
+    break
+  else
+    feed = client.get_feed(feed.next_link);
+  end
+end
+
+out_fp.close
