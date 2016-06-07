fb0af69b81c497d8e74cd71ef36bfc590331d8a2
msyksphinz <msyksphinz.dev@gmail.com>
2016-06-07 17:08:17 +0000

    Func: add bazel sample

--- bazel_test/app1/app1.cpp
+++ bazel_test/app1/app1.cpp
@@ -1,8 +1,11 @@
 #include <stdio.h>
-#include "app2.hpp"
+#include "app2/app2.hpp"
+#include "app3/app3.hpp"
 
 int main ()
 {
   printf ("App2 calling () ... %d\n", app2(1, 2));
+  printf ("HOGE is = %d\n", HOGE);
+
   return 0;
 }
