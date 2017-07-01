8c1e46057f7a5b37fde84ab6e06c8d52b093a0fb
Masayuki Kimura <masayuki@pezy.co.jp>
2017-07-01 11:12:39 +0900

    Func: update train_twolayernet

--- machine_learning/bp/train_twolayernet.c
+++ machine_learning/bp/train_twolayernet.c
@@ -20,10 +20,10 @@
 #define SEED     (65535)  // Seed of random
 #define MAXINPUTNO (60000)  // Max number of learning data
 #define BATCH_SIZE (100)
-#define BIGNUM     (100)  // Initial value of error
 #define LEARNING_RATE (0.1)
-#define LIMIT      (0.001)   // Max number of error
 #define WEIGHT_INIT (0.01)
+
+#define DUMP_IDX  (1)
 
 int open_image ();
 int open_label ();
@@ -61,7 +61,8 @@ double affine_backward (const int output_size,
 						double dw[output_size][hidden_size],
 						const double dout[batch_size][output_size],
 						const double w[hidden_size][output_size],
-						const double x[batch_size][hidden_size]);
+						const double x[batch_size][hidden_size],
+						int debug);
 
 void relu (const int batch_size,
 		   const int size,
@@ -84,6 +85,14 @@ double softmax_backward (const int batch_size,
 						 const double t[batch_size][size]);
 
 void forward (double *o, double *e, int input_size, int output_size);
+
+void TestNetwork (const int input_size,
+				  const int output_size,
+				  const int hidden_size,
+				  const double wh0[input_size][hidden_size],
+				  const double wb0[hidden_size],
+				  const double wh1[hidden_size][output_size],
+				  const double wb1[output_size]);
 
 int argmax (const int x_size, double *o);
 
@@ -116,36 +125,23 @@ int main ()
   double in_data[BATCH_SIZE][INPUTNO];
   double ans_data[BATCH_SIZE];
   double o[OUTPUTNO];
-  double err = BIGNUM;
-  int i, j;
   int n_of_e;
 
   srand (SEED);
 
-
-  initwh0 (wh0);
-  initwh1 (wh1);
-
-  initwb0 (wb0);
-  initwb1 (wb1);
-
-  // initwh (INPUTNO, HIDDENNO,  wh0);
-  // initwh (HIDDENNO, OUTPUTNO, wh1);
-  //
-  // initwb (HIDDENNO, wb0);
-  // initwb (HIDDENNO, wb1);
+  initwh (INPUTNO, HIDDENNO, wh0);
+  initwb (HIDDENNO, wb0);
+  initwh (HIDDENNO, OUTPUTNO, wh0);
+  initwb (OUTPUTNO, wb1);
 
   n_of_e = MAXINPUTNO / BATCH_SIZE;
   int fd_image = open_image ();
   int fd_label = open_label ();
 
-  // while (err > LIMIT) {
-  err = 0.0;
-  int correct = 0;
+  const int epoch_size = 50;
+  
   for (int no_input = 0; no_input < n_of_e; no_input++) {
 	getdata (fd_image, fd_label, in_data, ans_data);
-
-	// print_images (in_data[0], ans_data[0]);
 
 	double af0[BATCH_SIZE][HIDDENNO];
 	double af1[BATCH_SIZE][OUTPUTNO];
@@ -155,122 +151,55 @@ int main ()
 	affine (HIDDENNO, INPUTNO, BATCH_SIZE, af0, in_data, wh0, wb0);
 	relu (BATCH_SIZE, HIDDENNO, rel0, af0);
 
-    // printf ("affine0 : "); for (int i = 0; i < HIDDENNO; i++) { printf ("%1.10lf ", af0[i]); if ((i%10) == 9) { printf ("\n"); }  } printf("\n");
-    // printf ("relu0   : "); for (int i = 0; i < HIDDENNO; i++) { printf ("%1.3lf ", rel0[i]); } printf("\n");
-
-    affine (OUTPUTNO, HIDDENNO, BATCH_SIZE, af1, rel0,    wh1, wb1);
+    affine (OUTPUTNO, HIDDENNO, BATCH_SIZE, af1, rel0, wh1, wb1);
 	softmax (BATCH_SIZE, OUTPUTNO, rel1, af1);
-
-    // printf ("affine1 : "); for (int i = 0; i < OUTPUTNO; i++) { printf ("%1.5lf ", af1[i]); } printf("\n");
-
-	// printf ("affine1:\n");
-	// for (int b = 0; b < BATCH_SIZE; b++) {
-	//   for (int i = 0; i < OUTPUTNO; i++) { 
-	// 	printf ("%1.10lf ", af1[b][i]); 
-	//   } 
-	//   printf("\n");
-	// }
-	// 
-	// printf ("softmax1:\n");
-	// for (int b = 0; b < BATCH_SIZE; b++) {
-	//   for (int i = 0; i < OUTPUTNO; i++) { 
-	// 	printf ("%1.10lf ", rel1[b][i]); 
-	//   } 
-	//   printf("\n");
-	// }
-
-	for (int b = 0; b < BATCH_SIZE; b++) {
-	  int t = argmax (OUTPUTNO, af1[b]);
-	  if (t == (int)ans_data[b]) correct++;
-	}
 
 	// Back ward
 	double ans_label[BATCH_SIZE][OUTPUTNO] = {{0.0}};
 	for (int b = 0; b < BATCH_SIZE; b++) {
 	  ans_label[b][(int)ans_data[b]] = 1.0;
 	}
-
 	double softmax_dx[BATCH_SIZE][OUTPUTNO];
 	softmax_backward (BATCH_SIZE, OUTPUTNO, softmax_dx, rel1, ans_label);
-	
-	// printf ("softmax_dx : \n"); 
-	// for (int b = 0; b < BATCH_SIZE; b++) {
-	//   for (int o = 0; o < OUTPUTNO; o++) {
-	// 	printf ("%1.10lf ", softmax_dx[b][o]);
-	//   }
-	//   printf ("\n");
-	// }
-
-	// printf ("wh1 : \n"); 
-	// for (int h = 0; h < HIDDENNO; h++) {
-	//   for (int o = 0; o < OUTPUTNO; o++) {
-	// 	printf ("%1.10lf ", wh1[h][o]);
-	//   }
-	//   printf ("\n");
-	// }
-	// 
-	// 
-	// printf ("rel0 : \n"); 
-	// for (int b = 0; b < BATCH_SIZE; b++) {
-	//   for (int h = 0; h < HIDDENNO; h++) {
-	// 	printf ("%1.10lf ", rel0[b][h]);
-	//   }
-	//   printf ("\n");
-	// }
-
-
 	double affine1_dx[BATCH_SIZE][HIDDENNO];
 	double affine1_dw[HIDDENNO][OUTPUTNO];
 	double affine1_db[OUTPUTNO];
+
 	affine_backward (OUTPUTNO, HIDDENNO, BATCH_SIZE,
 					 affine1_dx, affine1_db, affine1_dw, 
-					 softmax_dx, wh1, rel0);
-
-	// printf ("affine1_dw : "); 
-	// for (int h = 0; h < HIDDENNO; h++) {
-	//   for (int o = 0; o < OUTPUTNO; o++) {
-	// 	printf ("%1.10lf ", affine1_dw[h][o]);
-	//   }
-	//   printf ("\n");
-	// }
-
-	// printf ("affine1_dx : "); 
-	// for (int b = 0; b < BATCH_SIZE; b++) {
-	//   for (int h = 0; h < HIDDENNO; h++) {
-	// 	printf ("%1.10lf ", affine1_dx[b][h]);
-	//   }
-	//   printf ("\n");
-	// }
+					 softmax_dx, wh1, rel0, no_input == DUMP_IDX);
 
 	double relu_dx[BATCH_SIZE][HIDDENNO];
 	relu_backward (BATCH_SIZE, HIDDENNO, relu_dx, af0, affine1_dx);
-
-	// printf ("relu_dx : \n");
-	// for (int b = 0; b < BATCH_SIZE; b++) {
-	//   for (int h = 0; h < HIDDENNO; h++) {
-	// 	printf ("%1.10lf ", relu_dx[b][h]);
-	//   }
-	//   printf ("\n");
-	// }
-
-
 	double affine0_dx[BATCH_SIZE][INPUTNO];
 	double affine0_dw[INPUTNO][HIDDENNO];
 	double affine0_db[HIDDENNO];
 	affine_backward (HIDDENNO, INPUTNO, BATCH_SIZE,
 					 affine0_dx, affine0_db, affine0_dw, 
-					 relu_dx, wh0, in_data);
+					 relu_dx, wh0, in_data, 0);
 
-	// printf ("affine0_dw : "); 
-	// for (int i = 0; i < INPUTNO; i++) {
-	//   for (int h = 0; h < HIDDENNO; h++) {
-	// 	printf ("%1.10lf ", affine0_dw[i][h]);
-	//   }
-	//   printf ("\n");
-	// }
+	for (int i = 0; i < INPUTNO; i++) {
+	  for (int h = 0; h < HIDDENNO; h++) {
+		wh0[i][h] -= (LEARNING_RATE * affine0_dw[i][h]);
+	  }
+	}
 
-	err = (double)correct / (no_input * BATCH_SIZE);
-	printf ("%d\t%lf\n", no_input, err);
+	for (int h = 0; h < HIDDENNO; h++) {
+	  wb0[h] -= (LEARNING_RATE * affine0_db[h]);
+	}
+	
+	for (int h = 0; h < HIDDENNO; h++) {
+	  for (int o = 0; o < OUTPUTNO; o++) {
+		wh1[h][o] -= (LEARNING_RATE * affine1_dw[h][o]);
+	  }
+	}
+	for (int o = 0; o < OUTPUTNO; o++) {
+	  wb1[o] -= (LEARNING_RATE * affine1_db[o]);
+	}
+	
+	if ((no_input % epoch_size) == 0) {
+	  TestNetwork (INPUTNO, OUTPUTNO, HIDDENNO, wh0, wb0, wh1, wb1);
+	}
   }
 
   // }
@@ -280,6 +209,72 @@ int main ()
 
   return 0;
 }
+
+void TestNetwork (const int input_size,
+				  const int output_size,
+				  const int hidden_size,
+				  const double wh0[input_size][hidden_size],
+				  const double wb0[hidden_size],
+				  const double wh1[hidden_size][output_size],
+				  const double wb1[output_size])
+{
+  printf ("=== TestNetwork ===\n");
+  int image_fd;
+  if ((image_fd = open("t10k-images-idx3-ubyte", O_RDONLY))==-1){
+	printf("couldn't open image file t10k-images-idx3-ubyte");
+	exit(0);
+  }
+  unsigned char *ptr;
+  static int num[10];
+  read(image_fd, num, 4 * sizeof(int));
+  
+  int label_fd;
+  if ((label_fd = open("t10k-labels-idx1-ubyte", O_RDONLY)) == -1){
+	printf("couldn't open image file t10k-labels-idx1-ubyte");
+	exit(0);
+  }
+  read(label_fd, num, 2 * sizeof(int));
+
+  double in_data[BATCH_SIZE][INPUTNO];
+  double ans_data[BATCH_SIZE];
+
+  int correct = 0;
+  
+  for (int no_input = 0; no_input < 10000; no_input += BATCH_SIZE) {
+	for (int b = 0; b < BATCH_SIZE; b++) {
+	  uint8_t image[INPUTNO];
+	  read (image_fd, image, INPUTNO * sizeof(unsigned char));
+	  for (int i = 0; i < INPUTNO; i++) {
+		in_data[b][i] = (double)image[i] / 255.0;
+	  }
+	  uint8_t label;
+	  read (label_fd, &label, sizeof(uint8_t));
+	  ans_data[b] = (double)label;
+	}
+	// if (no_input == 10000-BATCH_SIZE) print_images (in_data[0], ans_data[0]);
+
+	double af0[BATCH_SIZE][HIDDENNO];
+	double af1[BATCH_SIZE][OUTPUTNO];
+	double rel0[BATCH_SIZE][HIDDENNO];
+	double rel1[BATCH_SIZE][OUTPUTNO];
+
+	affine (HIDDENNO, INPUTNO,  BATCH_SIZE, af0, in_data, wh0, wb0);
+	relu (BATCH_SIZE, HIDDENNO, rel0, af0);
+	affine (OUTPUTNO, HIDDENNO, BATCH_SIZE, af1, rel0,    wh1, wb1);
+
+	for (int b = 0; b < BATCH_SIZE; b++) {
+	  int t = argmax (OUTPUTNO, af1[b]);
+	  if (t == (int)ans_data[b]) correct++;
+	}
+  }
+  printf ("Correct = %d\n", correct);
+
+  close (image_fd);
+  close (label_fd);
+
+  return;
+}
+
 
 
 /* http://www.kk.iij4u.or.jp/~kondo/wave/swab.html */
@@ -350,7 +345,7 @@ int getdata (int fd_image, int fd_label, double in_data[BATCH_SIZE][INPUTNO], do
   for (int b = 0; b < BATCH_SIZE; b++) {
 	read (fd_image, image, INPUTNO * sizeof(unsigned char));
 	for (int j = 0; j < INPUTNO; j++) {
-	  in_data[b][j] = (double)image[j] / 256.0;
+	  in_data[b][j] = (float)image[j] / 255.0;
 	}
 	uint8_t label;
 	read (fd_label, &label, sizeof(uint8_t));
@@ -364,7 +359,7 @@ void print_images (double data[INPUTNO], double label)
   printf ("=== LABEL %d ===\n", (int)label);
   for (int j = 0; j < INPUTNO; j++) {
 	// printf("%3d ", (int)(data[j] * 256.0));
-    printf("%1.5lf ", data[j]);
+    printf("%1.5f ", (float)data[j]);
 	if ( (j+1) % 28 == 0 ){
 	  printf("\n");
 	}
@@ -375,18 +370,18 @@ void print_images (double data[INPUTNO], double label)
 double affine (const int output_size,
 			   const int input_size,
 			   const int batch_size,
-			   double o[batch_size][output_size],
+			   double out[batch_size][output_size],
 			   const double in_data[batch_size][input_size],
 			   const double wh[input_size][output_size],
 			   const double wb[output_size])
 {
   for (int b = 0; b < batch_size; b++) {
-	for (int i = 0; i < output_size; i++) {
-	  o[b][i] = 0.0;
-	  for (int j = 0; j < input_size; j++) {
-		o[b][i] += (in_data[b][j] * wh[j][i]);
+	for (int o = 0; o < output_size; o++) {
+	  out[b][o] = 0.0;
+	  for (int i = 0; i < input_size; i++) {
+		out[b][o] += (in_data[b][i] * wh[i][o]);
 	  }
-	  o[b][i] += wb[i];
+	  out[b][o] += wb[o];
 	}
   }
 }
@@ -400,7 +395,8 @@ double affine_backward (const int output_size,
 						double dw[hidden_size][output_size],
 						const double dout[batch_size][output_size],
 						const double w[hidden_size][output_size],
-						const double x[batch_size][hidden_size])
+						const double x[batch_size][hidden_size],
+						const int debug)
 {
   for (int b = 0; b < batch_size; b++) {
 	for (int y = 0; y < hidden_size; y++) {
@@ -412,6 +408,7 @@ double affine_backward (const int output_size,
   }
   for (int h = 0; h < hidden_size; h++) {
 	for (int o = 0; o < output_size; o++) {
+	  dw[h][o] = 0.0;
 	  for (int b = 0; b < batch_size; b++) {
 		dw[h][o] += (x[b][h] * dout[b][o]);
 	  }
@@ -488,17 +485,10 @@ double softmax_backward (const int batch_size,
 						 const double y[batch_size][size],
 						 const double t[batch_size][size])
 {
-  // printf ("=== softmax_backward ===\n");
   for (int b = 0; b < batch_size; b++) {
-	// for (int y_idx = 0; y_idx < size; y_idx++) {
-	//   printf ("%1.10lf ", t[b][y_idx]);
-	// }
-	// printf ("\n");
 	for (int y_idx = 0; y_idx < size; y_idx++) {
 	  dx[b][y_idx] = (y[b][y_idx] - t[b][y_idx]) / batch_size;
-	  // printf ("%1.10lf ", dx[b][y_idx]);
 	}
-	// printf ("\n");
   }
 }
 
