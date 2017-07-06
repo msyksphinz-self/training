3d9e2cb408a4c4ac39abb472bd0563eac2d65400
msyksphinz <msyksphinz.dev@gmail.com>
2017-07-06 23:15:19 +0900

    Func: add MNIST program perf (refs #126)

--- machine_learning/bp/train_twolayernet_float.c
+++ machine_learning/bp/train_twolayernet_float.c
@@ -13,6 +13,10 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+#ifdef GPERF
+#include <gperftools/profiler.h>
+#endif // GPERF
+
 #define INPUTNO  (28*28)    // No of input cell
 #define OUTPUTNO (10)
 #define HIDDENNO (50)    // No of hidden cell
@@ -136,6 +140,10 @@ int main ()
 
   getdata (fd_image, fd_label, in_data, ans_data);
 
+#ifdef GPERF
+  ProfilerStart("train_twolayernet_float.prof");
+#endif // GPERF
+  
   for (int no_input = 0; no_input < n_of_e; no_input++) {
 
     float af0 [BATCH_SIZE * HIDDENNO];
@@ -197,6 +205,10 @@ int main ()
 	}
   }
 
+#ifdef GPERF
+  ProfilerStop ();
+#endif // GPERF
+  
   // }
 
   close(fd_image);
