44e772650acc170d085303f3f156d4d2f2eafdd8
msyksphinz <msyksphinz.dev@gmail.com>
2018-03-01 23:25:27 +0900

    Func: add haribote-002

---
+++ program/haribote-os/day04/haribote-002/bootpack.c
@@ -0,0 +1,97 @@
+#define COL8_000000  0
+#define COL8_FF0000  1
+#define COL8_00FF00  2
+#define COL8_FFFF00  3
+#define COL8_0000FF  4
+#define COL8_FF00FF  5
+#define COL8_00FFFF  6
+#define COL8_FFFFFF  7
+#define COL8_C6C6C6  8
+#define COL8_840000  9
+#define COL8_008400 10
+#define COL8_848400 11
+#define COL8_000084 12
+#define COL8_840084 13
+#define COL8_008484 14
+#define COL8_848484 15
+
+
+void io_hlt(void);
+void io_cli(void);
+void io_out8(int port, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
+
+void HariMain(void)
+{
+  int i;
+  char *p;
+
+  init_pallete();
+  p = (char *) 0xa0000;
+  boxfill8(p, 320, COL8_FF0000,  20, 20, 120, 120);
+  boxfill8(p, 320, COL8_00FF00,  70, 50, 170, 150);
+  boxfill8(p, 320, COL8_0000FF, 120, 80, 220, 180);
+  
+  for (;;) {
+    io_hlt();
+  }
+}
+
+
+void init_pallete(void)
+{
+  static unsigned char table_rgb[16 * 3] = {
+	0x00, 0x00, 0x00,   /*  0: Black       	*/
+	0xff, 0x00, 0x00,   /*  1: Light Read  	*/
+	0x00, 0xff, 0x00,   /*  2: Light Green 	*/
+	0xff, 0xff, 0x00,   /*  3: Light Yellow */
+	0x00, 0x00, 0xff,   /*  4: Light Blue   */
+	0xff, 0x00, 0xff,   /*  5: Light Purple */
+	0x00, 0xff, 0xff,   /*  6: Light Light Blue */
+	0xff, 0xff, 0xff,   /*  7: White        */
+	0xc6, 0xc6, 0xc6,   /*  8: Light Gray   */
+	0x84, 0x00, 0x00,   /*  9: Dark Red     */
+	0x00, 0x84, 0x00,   /* 10: Dark Green   */
+	0x84, 0x84, 0x00,   /* 11: Dark Yellow  */
+	0x00, 0x00, 0x84,   /* 12: Dark Blue    */
+	0x84, 0x00, 0x84,   /* 13: Dark Purple  */
+	0x00, 0x84, 0x84,   /* 14: Dark Light Blue */
+	0x84, 0x84, 0x84    /* 15: Dark Gray    */
+  };
+  set_palette (0, 15, table_rgb);
+  return;
+
+}
+
+
+void set_palette (int start, int end, unsigned char *rgb)
+{
+  int i, eflags;
+  eflags = io_load_eflags ();
+  io_cli ();                  /* clear interrupt flag */
+  io_out8 (0x03c8, start);
+  for (i = start; i <= end; i++) {
+	io_out8(0x03c9, rgb[0]);
+	io_out8(0x03c9, rgb[1]);
+	io_out8(0x03c9, rgb[2]);
+	rgb += 3;
+  }
+  io_store_eflags (eflags);
+  return;
+}
+
+  
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1)
+{
+  int x, y;
+  for (y = y0; y <= y1; y++) {
+	for (x = x0; x <= x1; x++) {
+	  vram[y * xsize + x] = c;
+	}
+  }
+  return;
+}
+
+
+
