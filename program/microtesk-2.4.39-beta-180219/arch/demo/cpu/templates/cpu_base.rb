497874b152edbc11913857333fe2642a96f4814e
msyksphinz <msyksphinz.dev@gmail.com>
2018-03-23 22:28:32 +0900

    Func: cpu_base.rb

--- program/microtesk-2.4.39-beta-180219/arch/demo/cpu/templates/cpu_base.rb
+++ program/microtesk-2.4.39-beta-180219/arch/demo/cpu/templates/cpu_base.rb
@@ -66,9 +66,9 @@ class CpuBaseTemplate < Template
       comment 'Initializer for REG: %d', value
 
       mov target, imm(value(2, 7))
-      add target, target
-      add target, target
-      add target, imm(value(0, 1))
+      # add target, target
+      # add target, target
+      # add target, imm(value(0, 1))
     }
 
     preparator(:target => 'REG', :mask => "'b00XX_XXXX") {
