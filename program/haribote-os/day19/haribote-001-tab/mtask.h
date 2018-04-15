66c099af2da9933db6cf6092c745c519a4ec9874
msyksphinz <msyksphinz.dev@gmail.com>
2018-04-15 16:07:28 +0900

    Func: add haribote day19 haribote-001 tab

---
+++ program/haribote-os/day19/haribote-001-tab/mtask.h
@@ -0,0 +1,73 @@
+#ifndef __MTASK_H__
+#define __MTASK_H__
+
+#include <stdint.h>
+#include "bootpack.h"
+#include "memory.h"
+#include "fifo.h"
+
+#define MAX_TASKS      1000
+#define MAX_TASKS_LV   100
+#define MAX_TASKLEVELS 10
+#define TASK_GDT0      3
+
+
+/* Task TSS */
+struct TSS32 {
+  int backlink, esp0, ss0, esp1, ss1, esp2, ss2, cr3;
+  int eip, eflags, eax, ecx, edx, ebx, esp, ebp, esi, edi;
+  int es, cs, ss, ds, fs, gs;
+  int ldtr, iomap;
+};
+
+struct FIFO32 {
+  int *buf;
+  int p, q, size, free, flags;
+  struct TASK *task;
+};
+
+struct TASK {
+  int sel, flags;   // sel = GDT index
+  int level, priority;
+  struct FIFO32 fifo;
+  struct TSS32 tss;
+};
+
+struct TASKLEVEL {
+  int running;
+  int now;
+  struct TASK *tasks[MAX_TASKS_LV];
+};
+
+
+struct TASKCTL {
+  int  now_lvl;   /* Number of running level */
+  char lv_change;       /* Next task switch, change level or not */
+  struct TASKLEVEL level[MAX_TASKLEVELS];
+  struct TASK tasks0[MAX_TASKS];
+};
+
+struct TASK *task_init (struct MEMMAN *memman);
+struct TASK *task_alloc(void);
+void task_run (struct TASK *task, int level, int priority);
+void task_switch(void);
+void task_sleep (struct TASK *task);
+struct TASK *task_now (void);
+void task_add (struct TASK *task);
+void task_remove (struct TASK *task);
+void task_switchsub (void);
+void task_idle(void);
+
+void console_task (struct SHEET *sheet, unsigned int memtotal);
+int cons_newline (int cursor_y, struct SHEET *sheet);
+
+struct FILEINFO {
+  uint8_t  name[8], ext[3], type;
+  uint8_t  reserve[10];
+  uint16_t time, date, clustno;
+  uint32_t size;
+};
+
+
+#endif // __MTASK_H__
+
