5b01bb11f1e683bd370c61fc92001c5f36eb5d97
msyksphinz <msyksphinz.dev@gmail.com>
2018-04-21 13:37:54 +0900

    Func: add day20 haribote-002-irq

---
+++ program/haribote-os/day20/haribote-002-irq/console.h
@@ -0,0 +1,24 @@
+#ifndef __CONSOLE_H__
+#define __CONSOLE_H__
+
+#include "sheets.h"
+
+struct CONSOLE {
+  struct SHEET *sht;
+  int cur_x, cur_y, cur_c;
+};
+
+void console_task (struct SHEET *sheet, unsigned int memtotal);
+void cons_newline (struct CONSOLE *cons);
+
+void cons_runcmd (char *cmdline, struct CONSOLE *cons, int *fat, unsigned int memtotal);
+void cmd_mem (struct CONSOLE *cons, unsigned int memtotal);
+void cmd_cls (struct CONSOLE *cons);
+void cmd_dir (struct CONSOLE *cons);
+void cmd_type (struct CONSOLE *cons, int *fat, char *cmdline);
+void cmd_hlt (struct CONSOLE *cons, int *fat);
+void cons_putchar (struct CONSOLE *cons, int chr, char move);
+int cmd_app (struct CONSOLE *cons, int *fat, char *cmdline);
+    
+#endif // __CONSOLE_H__
+
