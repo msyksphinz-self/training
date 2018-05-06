d211317133b54dad434f3ac71c685e8bb8d116ae
msyksphinz <msyksphinz.dev@gmail.com>
2018-05-06 13:01:03 +0900

    Func: implement bootpack.c

--- program/haribote-os/day24/bootpack.c
+++ program/haribote-os/day24/bootpack.c
@@ -265,6 +265,9 @@ void HariMain(void)
           task_cons->tss.eip = (int) asm_end_app;
           io_sti ();
         }
+		if (i == 256 + 0x57 && shtctl->top > 2) {   /* F11 */
+		  sheet_updown (shtctl->sheets[1], shtctl->top-1);
+		}
         // Redraw cursor
         if (cursor_c >= 0) { 
           boxfill8 (sht_win->buf, sht_win->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
