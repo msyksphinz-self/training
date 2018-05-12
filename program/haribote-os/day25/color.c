6f21e76c0ee45a73521106c62ed707bde9dd5a66
msyksphinz <msyksphinz.dev@gmail.com>
2018-05-12 12:06:12 +0900

    Haribote: update color application

--- program/haribote-os/day25/color.c
+++ program/haribote-os/day25/color.c
@@ -1,4 +1,5 @@
 #include "a_nask.h"
+unsigned char rgb2pal (int r, int g, int b, int x, int y);
 
 void HariMain (void)
 {
@@ -10,7 +11,7 @@ void HariMain (void)
 	  int r = x * 2;
 	  int g = y * 2;
 	  int b = 0;
-	  buf [(x + 8) + (y + 28) * 144] = 16 + (r / 43) + (g / 43) * 6 + (b / 43) * 36;
+	  buf [(x + 8) + (y + 28) * 144] = rgb2pal(x * 2, y * 2, 0, x, y);
 	}
   }
   api_refreshwin (win, 8,28, 136, 156);
@@ -18,3 +19,17 @@ void HariMain (void)
   api_end();
 }
 
+unsigned char rgb2pal (int r, int g, int b, int x, int y)
+{
+  static int table[4] = {3, 1, 0, 2};
+  x &= 1;
+  y &= 1;
+  int i = table[x + y * 2];
+  r = (r * 21) / 256;
+  g = (g * 21) / 256;
+  b = (b * 21) / 256;
+  r = (r + i) / 4;
+  g = (g + i) / 4;
+  b = (b + i) / 4;
+  return 16 + r + g*6 + b*36;
+}
