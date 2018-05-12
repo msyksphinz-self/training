404c5ef47e040292183f4d384c4b4b4eae7f8b63
msyksphinz <msyksphinz.dev@gmail.com>
2018-05-12 12:49:55 +0900

    Haribote: support 2 windows

--- program/haribote-os/day25/color.c
+++ program/haribote-os/day25/color.c
@@ -1,32 +1,20 @@
 #include "a_nask.h"
-unsigned char rgb2pal (int r, int g, int b, int x, int y);
-
-void HariMain (void)
+void HariMain(void)
 {
-  api_initmalloc ();
-  char *buf = api_malloc (144 * 164);
-  int win = api_openwin (buf, 144, 164, -1, "color");
-  for (int y = 0; y < 128; y++) {
-	for (int x = 0; x < 128; x++) {
-	  buf [(x + 8) + (y + 28) * 144] = rgb2pal(x * 2, y * 2, 0, x, y);
+  char *buf;
+  int win, x, y, r, g, b;
+  api_initmalloc();
+  buf = api_malloc(144 * 164);
+  win = api_openwin(buf, 144, 164, -1, "color");
+  for (y = 0; y < 128; y++) {
+	for (x = 0; x < 128; x++) {
+	  r = x * 2;
+	  g = y * 2;
+	  b = 0;
+	  buf[(x + 8) + (y + 28) * 144] = 16 + (r / 43) + (g / 43) * 6 + (b / 43) * 36;
 	}
   }
-  api_refreshwin (win, 8,28, 136, 156);
-  api_getkey (1);
+  api_refreshwin(win, 8, 28, 136, 156);
+  api_getkey(1);
   api_end();
-}
-
-unsigned char rgb2pal (int r, int g, int b, int x, int y)
-{
-  static int table[4] = {3, 1, 0, 2};
-  x &= 1;
-  y &= 1;
-  int i = table[x + y * 2];
-  r = (r * 21) / 256;
-  g = (g * 21) / 256;
-  b = (b * 21) / 256;
-  r = (r + i) / 4;
-  g = (g + i) / 4;
-  b = (b + i) / 4;
-  return 16 + r + g*6 + b*36;
 }
