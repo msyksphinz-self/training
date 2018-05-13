a6c5df7bdf4a5342f5e78a1802c8bf1ad310b598
msyksphinz <msyksphinz.dev@gmail.com>
2018-05-13 16:22:01 +0900

    Haribote: improve refreshmap

--- program/haribote-os/day26/sheets.c
+++ program/haribote-os/day26/sheets.c
@@ -201,15 +201,26 @@ void sheet_refreshmap (struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1, i
     if (by0 < 0) { by0 = 0; }
     if (bx1 > sht->bxsize) { bx1 = sht->bxsize; }
     if (by1 > sht->bysize) { by1 = sht->bysize; }
-    for (by = by0; by < by1; by++) {
-      vy = sht->vy0 + by;
-      for (bx = bx0; bx < bx1; bx++) {
-        vx = sht->vx0 + bx;
-        if (buf[by * sht->bxsize + bx] != sht->col_inv) {
-          map[vy * ctl->xsize + vx] = sid;
-        }
-      }
-    }
+	if (sht->col_inv == -1) {
+	  // no skeleton
+	  for (by = by0; by < by1; by++) {
+		vy = sht->vy0 + by;
+		for (bx = bx0; bx < bx1; bx++) {
+		  vx = sht->vx0 + bx;
+		  map[vy * ctl->xsize + vx] = sid;
+		}
+	  }
+	} else {
+	  for (by = by0; by < by1; by++) {
+		vy = sht->vy0 + by;
+		for (bx = bx0; bx < bx1; bx++) {
+		  vx = sht->vx0 + bx;
+		  if (buf[by * sht->bxsize + bx] != sht->col_inv) {
+			map[vy * ctl->xsize + vx] = sid;
+		  }
+		}
+	  }
+	}
   }
   return;
 }
