23fedcd39100e41aa1ac049a50aab05bfc6ae5f7
msyksphinz <msyksphinz.dev@gmail.com>
2018-05-19 22:04:34 +0900

    Haribote: add sosu

--- program/haribote-os/day28/winhelo/winhelo.c
+++ program/haribote-os/day28/winhelo/winhelo.c
@@ -1,10 +1,15 @@
 #include "a_nask.h"
 
-char buf [150 * 50];
-
 void HariMain (void)
 {
+  char buf [150 * 50];
   int win;
   win = api_openwin (buf, 150, 50, -1, "hello");
+  for (;;) {
+	if (api_getkey(1) == 0x0a) {
+	  break;
+	}
+  }
+  
   api_end ();
 }
