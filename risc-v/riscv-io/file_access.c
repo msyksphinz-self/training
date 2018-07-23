14401d891fd2faf8919d4ea0649228a943aaa50d
msyksphinz <msyksphinz.dev@gmail.com>
2018-07-24 02:11:43 +0900

    Func: add file_access test for RISC-V

---
+++ risc-v/riscv-io/file_access.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+  printf("Hello World\n");
+
+  FILE *fp;
+  if ((fp = fopen("sample_hello.txt", "w")) == NULL) {
+    perror ("sample_hello.txt");
+    exit(EXIT_FAILURE);
+  }
+  fprintf (fp, "Hello World\n");
+  fclose (fp);
+
+  if ((fp = fopen("sample_hello.txt", "r")) == NULL) {
+    perror ("sample_hello.txt");
+    exit(EXIT_FAILURE);
+  }
+  char cin[100];
+  fscanf (fp, "%s", cin);
+  printf ("%s\n", cin);
+
+  char c[100];
+  scanf("%s", c);
+  printf("%s\n", c);
+
+  return 0;
+}
