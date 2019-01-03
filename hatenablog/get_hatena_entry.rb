62f838dd8f0be51e69c3dde976a2d2126f21a718
msyksphinz <msyksphinz_dev.gmail.com>
2019-01-03 22:40:31 +0900

    Update get_hatena_entry.rb

--- hatenablog/get_hatena_entry.rb
+++ hatenablog/get_hatena_entry.rb
@@ -7,18 +7,18 @@ POST_URI = "https://blog.hatena.ne.jp/msyksphinz/msyksphinz.hatenablog.com/atom/
 
 auth = Atompub::Auth::Wsse.new(
   username: 'msyksphinz',
-  password: 'l5l3j1mx7h'
+  password: ''
 )
 client = Atompub::Client.new(auth: auth)
 
 feed = client.get_feed(POST_URI);
 
-out_fp = File.open("output.txt", "w")
+out_fp = File.open("output.md", "w")
 
 while feed != nil do
   entry = feed.entries
   entry.each{|content|
-    out_fp.printf("<ul><li><a href=\"%s\">%s</a></li></ul>\n", content.links[1].href, content.title)
+    out_fp.printf("- [%s (%s)](%s)\n", content.title, content.updated.strftime("%y/%m/%d"), content.links[1].href)
   }
 
   if feed.next_link == nil then
