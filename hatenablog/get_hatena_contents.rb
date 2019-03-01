089d416edab7d3052af24e3e3554256553c66f2e
msyksphinz <msyksphinz.dev@gmail.com>
2019-03-02 00:28:40 +0900

    Func: add get_hatena_contents.rb

---
+++ hatenablog/get_hatena_contents.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+# -*- encoding: utf-8 -*-
+
+require 'atomutil'
+
+POST_URI = "https://blog.hatena.ne.jp/msyksphinz/msyksphinz.hatenablog.com/atom/entry"
+
+auth = Atompub::Auth::Wsse.new(
+  username: 'msyksphinz',
+  password: ''
+)
+client = Atompub::Client.new(auth: auth)
+
+feed = client.get_feed(POST_URI);
+
+out_fp = File.open("contents.md", "w")
+
+while feed != nil do
+  entry = feed.entries
+  entry.each{|content|
+    out_fp.printf("===\n")
+    out_fp.printf("- [%s (%s)](%s)\n", content.title, content.updated.strftime("%y/%m/%d"), content.links[1].href)
+    out_fp.printf("%s\n", content.content)
+    out_fp.printf("===\n")
+  }
+
+  if feed.next_link == nil then
+    break
+  else
+    feed = client.get_feed(feed.next_link);
+  end
+end
+
+out_fp.close
