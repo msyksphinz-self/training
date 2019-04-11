860f1bfea39960f0fb07bf03a5737180ceb815ce
Masayuki Kimura <masayuki@pezy.co.jp>
2019-04-11 09:37:07 +0900

    Func: add c_multiple_clocks

---
+++ program/atcoder/beginner_120/c_multiple_clocks/utils.h
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <vector>
+
+#define ipair pair<int,int>
+#define rep(I,N) for(int I=0; I<N; I++)
+#define repj(i,j,n) for(int i=j;i<n;i++)
+
+#define PB(a,i) (a).push_back(i)
+#define ALL(a) (a).begin(),(a).end()
+
+#define OK(i) cout<<"OK "<<i<<endl
+#define Yes cout<<"Yes"<<endl
+#define YES cout<<"YES"<<endl
+#define No cout<<"No"<<endl
+#define NO cout<<"NO"<<endl
+
+typedef long long int lli;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector< vector<int> > Matrix;
